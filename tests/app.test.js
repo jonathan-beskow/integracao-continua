9f6b05d3293bf73694b46da1f58ab3123652466a
M373746 <j.beskow@sistran.com.br>
2024-07-29 15:56:10 -0300

    integracao continua

--- tests/app.test.js
+++ tests/app.test.js
@@ -3,9 +3,9 @@ const request = require('supertest');
 const app = require('../app');
 
 describe('GET /', () => {
-  it('should return Hello World!!!', async () => {
+  it('should return Hello World!', async () => {
     const res = await request(app).get('/');
     expect(res.statusCode).toEqual(200);
-    expect(res.text).toBe('Hello World!!!');
+    expect(res.text).toBe('Hello World!');
   });
 });
